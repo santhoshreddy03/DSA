9b919adc2478b2652e7bb9065c68115aaa2cfee1
Padala Santosh Reddy <105787437+santhoshreddy03@users.noreply.github.com>
2024-06-19 23:12:10 +0530

    Time: 51 ms (48.01%), Space: 16.7 MB (95.01%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div496-next-greater-element-i/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div496-next-greater-element-i.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div496-next-greater-element-i/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div496-next-greater-element-i.py
@@ -1,16 +1,34 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        
+        # o(m+n)
         nums1idx={}
+        stack=[]
         res=[-1]*len(nums1)
         for i in range(len(nums1)):
             nums1idx[nums1[i]]=i
-        
         for i in range(len(nums2)):
-            if nums2[i] not in nums1idx:
-                continue
-            for j in range(i+1,len(nums2)):
-                if nums2[j]>nums2[i]:
-                    idx=nums1idx[nums2[i]]
-                    res[idx]=nums2[j]
-                    break
+            while stack and nums2[i]>stack[-1]:
+                val=stack.pop()
+                idx=nums1idx[val]
+                res[idx]=nums2[i]
+            if nums2[i] in nums1idx:
+                stack.append(nums2[i])
         return res
+        
+#         o(m*n)
+#         nums1idx={}
+#         res=[-1]*len(nums1)
+#         for i in range(len(nums1)):
+#             nums1idx[nums1[i]]=i
+        
+#         for i in range(len(nums2)):
+#             if nums2[i] not in nums1idx:
+#                 continue
+#             for j in range(i+1,len(nums2)):
+#                 if nums2[j]>nums2[i]:
+#                     idx=nums1idx[nums2[i]]
+#                     res[idx]=nums2[j]
+#                     break
+#         return res
