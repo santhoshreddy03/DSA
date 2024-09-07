fb12638d7aeb77547282f5e9f8f10af2d9e065c6
Padala Santosh Reddy <105787437+santhoshreddy03@users.noreply.github.com>
2024-09-07 20:09:35 +0530

    Time: 35 ms (72.41%), Space: 16.7 MB (68.97%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
         res=[]
-        
-        def sub(ind,curr):
-            if ind>=len(nums):
+        def sub(i,curr):
+            if i>=len(nums):
                 return res.append(curr[:])
             
-            curr.append(nums[ind])
-            sub(ind+1,curr)
+            curr.append(nums[i])
+            sub(i+1,curr)
             curr.pop()
-            sub(ind+1,curr)
+            sub(i+1,curr)
             
         sub(0,[])
         return res
